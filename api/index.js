2b866c3f0cd3a2424dbc16fc474df5c7d9836832
Aluízio Catão <aluizio2210@gmail.com>
2022-06-29 16:45:07 -0500

    Comentários no index.js

--- api/index.js
+++ api/index.js
@@ -1,20 +1,24 @@
+// Importações dos módulos htpp, e url para suas respectivas variáreis htpp e URL.
 const http = require('http')
 const data = require('./url.json')
 const URL = require('url')
 
+// Criação de um servidor
 http.createServer((req, res) => {
     // res.end(JSON.stringify(data))
 
+    // Preenchimento das variavéis com os dados passado na url
     const {name, url, del} = URL.parse(req.url,true).query
 
+    // Verifica se não existe o NAME e a URL na passagem dos dados pela URL.
     if(!name || !url){
         return res.end('show')
     }
-
+    // Verifica se existe o DEL na passagem dos dados pela URL
     if(del) {
         return res.end('delete')
     }
-
+    // Retorno obtido caso os dados nas variáveis NAME e URL existam. 
     return res.end('create')
 
 }).listen(3000, () => console.log('API is running'));
